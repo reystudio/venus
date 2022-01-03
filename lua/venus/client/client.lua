c17b6f8c864f0c171bd9abdd473c41aa504741a4
Kenneth R <reystudioo@yahoo.com>
2022-01-03 04:23:19 +0300

    removed modules i wont use in future

--- lua/venus/client/client.lua
+++
@@ -1,7 +0,0 @@
-if SERVER then
-    AddCSLuaFile 'msg.lua'
-    print('Loading clientside')
-else
-    include 'msg.lua'
-    print('Clientside initialized')
-end
--- lua/venus/client/msg.lua
+++
@@ -1,3 +0,0 @@
-net.Receive('venus.msg', function()
-	chat.AddText(unpack(net.ReadTable()))
-end)
